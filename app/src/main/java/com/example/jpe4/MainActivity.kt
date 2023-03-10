96732c29ef58ef32575a76c0476c481bfed08599
Surya <surya@wacmob.com>
2023-03-10 10:36:45 +0530

    Change MainActivity

--- app/src/main/java/com/example/jpe4/MainActivity.kt
+++ app/src/main/java/com/example/jpe4/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JPE4Theme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
